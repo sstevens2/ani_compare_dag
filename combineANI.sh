5fb40326af81d09612507db14ac7596faa223f11
Sarah Stevens <sstevens2@wisc.edu>
2018-04-26 17:06:28 -0500

    Fixed the combining one output when there were lots of comparisons issue

--- combineANI.sh
+++ combineANI.sh
@@ -2,8 +2,15 @@
 
 while read line
 do
-mv $line-*.ani.out $line
-tail -n 2 $line/$line-*.ani.out > $line.all.ani.out
+mkdir $line/anioutput/
+while read combo;
+do 
+sub=`echo $combo | cut -d',' -f1`
+query=`echo $combo | cut -d',' -f2`
+output=$line-$sub-$query.ani.out
+mv $output $line/anioutput
+done < $line/ani_combos_$line.txt
+tail -n 2 $line/anioutput/*.ani.out > $line.all.ani.out
 num=`wc -l $line/ani_combos_$line.txt | cut -f1 -d' '`
 cat $line.all.ani.out | sort | uniq | sed '/^\s*$/d' | head -n $num > $line.all.ani.out.cleaned
 done < $1
