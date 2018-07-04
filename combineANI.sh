83dec46ebda8ca9fbeb1343f6067a2138df188d6
Sarah Stevens <sstevens2@wisc.edu>
2018-07-03 21:01:21 -0500

    Update combineANI.sh

--- combineANI.sh
+++ combineANI.sh
@@ -9,8 +9,8 @@ sub=`echo $combo | cut -d',' -f1`
 query=`echo $combo | cut -d',' -f2`
 output=$line-$sub-$query.ani.out
 mv $output $line/anioutput
+tail -n 2 $line/anioutput/$output >> $line.all.ani.out
 done < $line/ani_combos_$line.txt
-tail -n 2 $line/anioutput/*.ani.out > $line.all.ani.out
 num=`wc -l $line/ani_combos_$line.txt | cut -f1 -d' '`
 cat $line.all.ani.out | sort | uniq | sed '/^\s*$/d' | head -n $num > $line.all.ani.out.cleaned
 done < $1
